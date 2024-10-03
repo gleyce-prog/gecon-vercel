f42ec5cce8b31cbdff9a1bd80e4c4d59c59cec4b
gleyce-prog <80989757+gleyce-prog@users.noreply.github.com>
2024-10-03 20:12:37 +0000

    inserindo arquivos

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -11,3 +11,21 @@ export const api = (withAuth = false) => axios.create({
   },
   data: {}
 })
+
+export default async function handler(req, res) {
+  const apiUrl = 'http://195.35.40.77:8080' + req.url;
+  try {
+    const response = await fetch(apiUrl, {
+      method: req.method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...req.headers
+      },
+      body: req.method !== 'GET' ? JSON.stringify(req.body) : null
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao fazer requisição para a API.' });
+  }
+}
