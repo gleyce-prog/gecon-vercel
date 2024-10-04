3ab473f1fe9c9999eb7ee63dc25a0a0e42faa988
gleyce-prog <80989757+gleyce-prog@users.noreply.github.com>
2024-10-04 11:08:37 +0000

    ultimas

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RoutesProtected } from './config/Rotas_protegidas';
-import { getBasename, setBasenameInLocalStorage, getBasenameInLocalStorage} from './config/getBasename';
-
-const basename = getBasename();
-setBasenameInLocalStorage(basename);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={getBasenameInLocalStorage()}>
+  <BrowserRouter>
     <RoutesProtected />
   </BrowserRouter>
 );
