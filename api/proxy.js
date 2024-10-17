2cf7b81594060f7f1b7affa49ba1aff580f360ed
Gleyce <80989757+gleyce-prog@users.noreply.github.com>
2024-10-17 03:32:45 +0000

    Alterações

--- api/proxy.js
+++ api/proxy.js
@@ -1,25 +1,30 @@
 export default async function handler(req, res) {
   const base = req.url.includes('/api/proxy') ? req.url.replace('/api/proxy/', '') : req.url;
 
-  const endpoint = `http://195.35.40.77:8080/gecon/v1/${base}`;
-  console.log("Endpoint: " + endpoint)
-  console.log("Headers: "+ req.headers)
+  console.log("Endpoint: " + base);
+  console.log("Headers: ", req.headers);
 
   try {
-    const response = await fetch(endpoint, {
+    const response = await fetch(base, {
       method: req.method,
-      headers: req.headers,
+      headers: {
+        ...req.headers,
+        // Adicione cabeçalhos adicionais, se necessário, como Authorization
+      },
       body: req.method === 'POST' || req.method === 'PUT' ? JSON.stringify(req.body) : undefined,
     });
 
+    // Se a resposta não for bem-sucedida, trata os erros
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      const errorData = await response.json();
+      console.error('Erro na requisição:', errorData); // Exibe a mensagem de erro da resposta
+      return res.status(response.status).json({ error: errorData.message || 'Erro desconhecido' });
     }
 
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
     console.error('Erro na requisição:', error);
-    res.status(500).json({ error: error.message  });
+    res.status(500).json({ error: error.message || 'Erro interno do servidor' });
   }
 }
--- api/teste.js
+++ api/teste.js
@@ -1,4 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: "Rota de teste funcionando" });
-  }
-  
+  res.status(200).json({ message: "Rota de teste funcionando" });
+}
