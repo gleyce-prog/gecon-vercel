dc8334a2bef00ec817f986790749d78c534aa6e2
Gleyce <80989757+gleyce-prog@users.noreply.github.com>
2024-10-17 03:37:13 +0000

    proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,30 +1,25 @@
 export default async function handler(req, res) {
   const base = req.url.includes('/api/proxy') ? req.url.replace('/api/proxy/', '') : req.url;
 
-  console.log("Endpoint: " + base);
-  console.log("Headers: ", req.headers);
+
+  console.log("Endpoint: " + endpoint)
+  console.log("Headers: "+ req.headers)
 
   try {
-    const response = await fetch(base, {
+    const response = await fetch(req.url, {
       method: req.method,
-      headers: {
-        ...req.headers,
-        // Adicione cabeçalhos adicionais, se necessário, como Authorization
-      },
+      headers: req.headers,
       body: req.method === 'POST' || req.method === 'PUT' ? JSON.stringify(req.body) : undefined,
     });
 
-    // Se a resposta não for bem-sucedida, trata os erros
     if (!response.ok) {
-      const errorData = await response.json();
-      console.error('Erro na requisição:', errorData); // Exibe a mensagem de erro da resposta
-      return res.status(response.status).json({ error: errorData.message || 'Erro desconhecido' });
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
     console.error('Erro na requisição:', error);
-    res.status(500).json({ error: error.message || 'Erro interno do servidor' });
+    res.status(error.status).json({ error: error.message  });
   }
 }
