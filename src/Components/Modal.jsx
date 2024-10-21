46e8a6aef79521724d90862ebf97007a49f0c0fa
Gleyce <80989757+gleyce-prog@users.noreply.github.com>
2024-10-21 20:42:09 +0000

    teste

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,6 +3,7 @@ import Multiselect from 'multiselect-react-dropdown';
 import { Modal, Form, Row, Col, Button } from 'react-bootstrap';
 import { api } from '../lib/Axios';
 import { url, token } from '../config/Globals';
+import InputMask from 'react-input-mask';
 import axios from 'axios';
 import { mostrarAlertaSucesso } from '../lib/swal';
 
@@ -129,40 +130,13 @@ const DynamicModal = ({ show, onHide, fields, post, get, onSubmit, title }) => {
     try {
       console.table(data, { tableName: 'Dados enviados!!' });
 
-      fetch(`https://painel-ativa.vercel.app/api/proxy/${post}`, {
-        method: `${method}`,
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-      })
-        .then(response => {
-          if (!response.ok) {
-            return response.json().then(err => {
-              alert(err.error.mensagem);
-            }).catch((err) => {
-              alert(err.error.mensagem);
-            });
-          }
-          return response.json();
-        })
-        .then(data => {
-          if (data) {
-            mostrarAlertaSucesso('Sucesso', 'Usuário cadastrado com sucesso!', () => {
-              onHide();
-              setTimeout(() => {
-                window.location.reload();
-              }, 700);
-            });
-          }
-        })
-        .catch(error => {
-          alert(error);
-        })
-        .finally(() => {
-          setIsSubmitting(false); // Reset submitting status after the request
-        });
+      mostrarAlertaSucesso(`Usuário ${(title.trim().split(' ')[0]) === "Cadastro" ? "cadastrado" : "editado"} com sucesso!`, 'Sucesso', () => {
+        onHide();
+        setTimeout(() => {
+          window.location.reload();
+        }, 700);
+      });
+
     } catch (error) {
       console.error('Erro ao enviar formulário:', error);
       setIsSubmitting(false); // Reset submitting status on error
@@ -277,9 +251,9 @@ const DynamicModal = ({ show, onHide, fields, post, get, onSubmit, title }) => {
                         )
                       ) : field.type === 'custom' ? (
                         field.customComponent
-                      ) : field.type === 'mask' ? (  
-                        <MaskedInput
-                          mask={field.mask} 
+                      ) : field.type === 'mask' ? (
+                        <InputMask
+                          mask={field.mask}
                           value={formValues[field.name] ?? ''}
                           onChange={handleChange}
                           placeholder={field.placeholder}
