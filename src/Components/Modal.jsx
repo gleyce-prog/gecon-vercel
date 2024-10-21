45fd8a3343fb362de5d067a385710ee24692a60f
Gleyce <80989757+gleyce-prog@users.noreply.github.com>
2024-10-21 20:35:54 +0000

    teste

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -130,6 +130,26 @@ const DynamicModal = ({ show, onHide, fields, post, get, onSubmit, title }) => {
     try {
       console.table(data, { tableName: 'Dados enviados!!' });
 
+      // fetch(`https://painel-ativa.vercel.app/api/proxy/${post}`, {
+      //   method: `${method}`,
+      //   headers: {
+      //     'Authorization': `Bearer ${token}`,
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(data)
+      // })
+      //   .then(response => {
+      //     if (!response.ok) {
+      //       return response.json().then(err => {
+      //         alert(err.error.mensagem);
+      //       }).catch((err) => {
+      //         alert(err.error.mensagem);
+      //       });
+      //     }
+      //     return response.json();
+      //   })
+      //   .then(data => {
+      //     if (data) {
             mostrarAlertaSucesso('Sucesso', `Usuário ${(title.trim().split(' ')[0]) === "Cadastro" ? "cadastrado" : "editado"} com sucesso!`, () => {              onHide();
               setTimeout(() => {
                 window.location.reload();
